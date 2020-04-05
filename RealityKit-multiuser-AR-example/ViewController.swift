e6ad80d78b0fa17543b3a73f9a6f0e6e9a9d12ac
sun-23 <twinsamson@gmail.com>
2020-04-05 14:46:48 +0700

    create game ok

--- RealityKit-multiuser-AR-example/ViewController.swift
+++ RealityKit-multiuser-AR-example/ViewController.swift
@@ -8,18 +8,64 @@
 
 import UIKit
 import RealityKit
+import ARKit
 
 class ViewController: UIViewController {
     
     @IBOutlet var arView: ARView!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        //
-        // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
         
-        // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
+        setupARView()
+        
+        arView.session.delegate = self
+        
+        // Add tapGesture
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleTap(recognizer:)))
+        arView.addGestureRecognizer(tapGestureRecognizer)
+    }
+    
+    func setupARView() {
+        arView.automaticallyConfigureSession = false
+        
+        let config = ARWorldTrackingConfiguration()
+        config.planeDetection = [.horizontal, .vertical]
+        config.environmentTexturing = .automatic
+        config.isCollaborationEnabled = true
+        
+        arView.session.run(config)
+    }
+    
+    @objc func handleTap(recognizer: UITapGestureRecognizer) {
+        // anchor position is camera position
+        let laserAnchor = ARAnchor(name: "LaserRed", transform: arView!.cameraTransform.matrix)
+        arView.session.add(anchor: laserAnchor)
+    }
+    
+    func placeObject(name entityName:String, for anchor: ARAnchor) {
+        // Load entity
+        let LaserEntity = try! ModelEntity.load(named: entityName)
+        let anchorEntity = AnchorEntity(anchor: anchor)
+        anchorEntity.addChild(LaserEntity)
+        // add anchorEntity to scene
+        arView.scene.addAnchor(anchorEntity)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+            // after 0.55 second remove anchorEntity
+            self.arView.scene.removeAnchor(anchorEntity)
+        }
+        
+    }
+}
+
+extension ViewController: ARSessionDelegate {
+    func session(_ session: ARSession, didAdd anchors: [ARAnchor]) {
+        for anchor in anchors {
+            // if see anchor name LaserRed then add LaserRed Entity to scene
+            if let anchorName = anchor.name, anchorName == "LaserRed" {
+                placeObject(name: anchorName, for: anchor)
+            }
+        }
     }
 }
