20fa569b2d9ef9f9898b4c017cf47f01881d7675
sun-23 <twinsamson@gmail.com>
2020-04-05 17:39:19 +0700

    finish ok

--- RealityKit-multiuser-AR-example/ViewController.swift
+++ RealityKit-multiuser-AR-example/ViewController.swift
@@ -9,15 +9,20 @@
 import UIKit
 import RealityKit
 import ARKit
+import MultipeerSession
 
 class ViewController: UIViewController {
     
     @IBOutlet var arView: ARView!
     
+    var multipeerSession: MultipeerSession?
+    var sessionIDObservation: NSKeyValueObservation?
+    
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
         setupARView()
+        setupMultipeerSession()
         
         arView.session.delegate = self
         
@@ -37,6 +42,20 @@ class ViewController: UIViewController {
         arView.session.run(config)
     }
     
+    func setupMultipeerSession() {
+        // Use key-value observation to monitor your ARsession's identifier
+        sessionIDObservation = observe(\.arView.session.identifier, options: [.new]) { ( object, change) in
+            print("SessionID changed to: \(change.newValue!)")
+            // Tell all other peers about your ARSession's changed ID, so
+            // that they can keep track of which ARAnchors are yours.
+            guard let multipeerSession = self.multipeerSession else { return }
+            self.sendARSessionIDTo(peers: multipeerSession.connectedPeers)
+        }
+        
+        // Start looking for other players via MultiPeerConnectivity
+        multipeerSession = MultipeerSession(serviceName: "multiuser-ar", receivedDataHandler: self.receivedData, peerJoinedHandler: self.peerJoined, peerLeftHandler: self.peerLeft, peerDiscoveredHandler: self.peerDiscovered)
+    }
+        
     @objc func handleTap(recognizer: UITapGestureRecognizer) {
         // anchor position is camera position
         let laserAnchor = ARAnchor(name: "LaserRed", transform: arView!.cameraTransform.matrix)
@@ -66,6 +85,115 @@ extension ViewController: ARSessionDelegate {
             if let anchorName = anchor.name, anchorName == "LaserRed" {
                 placeObject(name: anchorName, for: anchor)
             }
+            
+            if let participantAnchor = anchor as? ARParticipantAnchor {
+                print("Successfully connect to other peer user")
+                
+                let anchorEntity = AnchorEntity(anchor: participantAnchor)
+                
+                let mesh = MeshResource.generateSphere(radius: 0.03)
+                let material = SimpleMaterial(color: UIColor.red, isMetallic: false)
+                
+                let peerSphere = ModelEntity(mesh: mesh, materials: [material])
+                
+                anchorEntity.addChild(peerSphere)
+                arView.scene.addAnchor(anchorEntity)
+            }
+        }
+    }
+}
+
+// MARK: - MultipeerSession
+
+extension ViewController {
+    private func sendARSessionIDTo(peers: [PeerID]) {
+        guard let multipeerSession = multipeerSession else { return }
+        let idString = arView.session.identifier.uuidString
+        let command = "SessionID:" + idString
+        if let commandData = command.data(using: .utf8) {
+            multipeerSession.sendToPeers(commandData, reliably: true, peers: peers)
+        }
+    }
+    
+    func receivedData(_ data: Data, from peer: PeerID) {
+        
+        guard let multipeerSession = multipeerSession else { return }
+        
+        // Get collaborationData to update arView
+        if let collaborationData = try? NSKeyedUnarchiver.unarchivedObject(ofClass: ARSession.CollaborationData.self, from: data) {
+            arView.session.update(with: collaborationData)
+            return
+        }
+        // ...
+        let sessionIDCommandString = "SessionID:"
+        if let commandString = String(data: data, encoding: .utf8), commandString.starts(with: sessionIDCommandString) {
+            let newSessionID = String(commandString[commandString.index(commandString.startIndex,
+                                                                     offsetBy: sessionIDCommandString.count)...])
+            // If this peer was using a different session ID before, remove all its associated anchors.
+            // This will remove the old participant anchor and its geometry from the scene.
+            if let oldSessionID = multipeerSession.peerSessionIDs[peer] {
+                removeAllAnchorsOriginatingFromARSessionWithID(oldSessionID)
+            }
+            
+            multipeerSession.peerSessionIDs[peer] = newSessionID
+        }
+    }
+    
+    func peerDiscovered(_ peer: PeerID) -> Bool {
+        guard let multipeerSession = multipeerSession else { return false }
+        
+        if multipeerSession.connectedPeers.count > 5 {
+            // Do not accept more than four users in the experience.
+            print("A 6 peer wants to join the experience.\nThis app is limited to 5 users.")
+            return false
+        } else {
+            return true
+        }
+    }
+    /// - Tag: PeerJoined
+    func peerJoined(_ peer: PeerID) {
+        print("""
+            A peer wants to join the experience.
+            Hold the phones next to each other.
+            """)
+        // Provide your session ID to the new user so they can keep track of your anchors.
+        sendARSessionIDTo(peers: [peer])
+    }
+    
+    func peerLeft(_ peer: PeerID) {
+        guard let multipeerSession = multipeerSession else {
+            return
+        }
+        print("A peer has left the shared experience.")
+        
+        // Remove all ARAnchors associated with the peer that just left the experience.
+        if let sessionID = multipeerSession.peerSessionIDs[peer] {
+            removeAllAnchorsOriginatingFromARSessionWithID(sessionID)
+            multipeerSession.peerSessionIDs.removeValue(forKey: peer)
+        }
+    }
+    
+    private func removeAllAnchorsOriginatingFromARSessionWithID(_ identifier: String) {
+        guard let frame = arView.session.currentFrame else { return }
+        for anchor in frame.anchors {
+            guard let anchorSessionID = anchor.sessionIdentifier else { continue }
+            if anchorSessionID.uuidString == identifier {
+                arView.session.remove(anchor: anchor)
+            }
+        }
+    }
+    
+    /// - Tag: DidOutputCollaborationData
+    func session(_ session: ARSession, didOutputCollaborationData data: ARSession.CollaborationData) {
+        guard let multipeerSession = multipeerSession else { return }
+        if !multipeerSession.connectedPeers.isEmpty {
+            guard let encodedData = try? NSKeyedArchiver.archivedData(withRootObject: data, requiringSecureCoding: true)
+            else { fatalError("Unexpectedly failed to encode collaboration data.") }
+            // Use reliable mode if the data is critical, and unreliable mode if the data is optional.
+            let dataIsCritical = data.priority == .critical
+            multipeerSession.sendToAllPeers(encodedData, reliably: dataIsCritical)
+        } else {
+            print("Deferred sending collaboration to later because there are no peers.")
         }
     }
 }
